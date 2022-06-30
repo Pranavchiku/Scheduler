bd46000ebc3f879761536538d9d3a3a90fdc4e5a
Pranavchiku <goswami.4@iitj.ac.in>
2022-06-30 23:56:01 +0530

    added-requirements.txt

--- home/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-start_time.append(datetime.timestamp(now1))
-    # duration.append(datetime.timestamp(resp2[i]["duration"]))
-
-    # then=resp2[i]["start_time"]
-    # timeLeft.append(then-(datetime.now()))
-    # event.append(resp2[i]["name"])
--- scheduler/settings.py
+++ scheduler/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d_3(dkzj-8dqb$vk82p9i=7b^b$y*3n-6c(y2jd8^9vs7q&+%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
