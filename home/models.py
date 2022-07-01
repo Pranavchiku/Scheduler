1d90b7520bd7ab3cbf3cef221f0c5f62d32709aa
Pranavchiku <goswami.4@iitj.ac.in>
2022-07-01 21:30:24 +0530

    updated-requirements.txt

--- home/models.py
+++ home/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
-
-
 class BackEnd(models.Model):
     start=models.CharField(max_length=200)
     dur=models.CharField(max_length=200)
