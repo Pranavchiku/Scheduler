260957fb00e0e99819ebd5e3fdb3d55f68c58f53
Pranavchiku <goswami.4@iitj.ac.in>
2022-07-01 21:10:52 +0530

    added-procfile-runtime.txt

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from decouple import config
-
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
